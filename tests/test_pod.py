cf69853e1919cbf160698c3abf4151441e004761
chris.perso@gmail.com <chris.perso@gmail.com@986c3d7b-1ff4-b555-b2c5-4a075b755c62>
2011-08-26 15:32:01 +0000

    - updated tests - algorithm now more robust: discarding ill-formed inputs

--- tests/test_pod.py
+++ tests/test_pod.py
@@ -23,12 +23,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
 import unittest
 import math
 import logging
+from decimal import Decimal
 
 import pod
 
 logging.basicConfig(level=logging.DEBUG)
 
 class PodTest(unittest.TestCase):
+  """
+  Testing the POD algorithm.
+  
+  """
 
   def test_decompose_001(self):
     """
@@ -177,5 +182,78 @@ class PodTest(unittest.TestCase):
     self.assertAlmostEqual(replication.get_reference_weight(2), 0.61538, 4)
     self.assertAlmostEqual(replication.get_error_norm(), 0.0, 4)
     
+  def prepare_fourier(self, shift):
+    N_REF = 30
+    SAMPLING = 5 * N_REF
+    AMPLITUTDE = 100.0
+    FREQ_STEP = 0.2
+    PHASE_SHIFT = shift
+    
+    def gen_x_axis(x0, n=SAMPLING):
+       return float(x0) * 2 * math.pi / n
+       
+    def square_signal(x0, ampl=AMPLITUTDE, shift=PHASE_SHIFT):
+       if x0 > math.pi + shift and x0 < 2.0 * math.pi + shift:
+          return 1.0 * ampl
+       else:
+          return 0.0
+    
+    def sine(ampl, freq, axis):
+       output = [ampl * math.sin(float(x0) * freq) for x0 in axis]
+       return output
+       
+    def cosine(ampl, freq, axis):
+       output = [ampl * math.cos(float(x0) * freq) for x0 in axis]
+       return output
+     
+    # x axis
+    x_axis = map(gen_x_axis, range(SAMPLING))
+      
+    # input signal
+    y = map(square_signal, x_axis)
+    
+    # creates set of sine and cosine functions
+    
+    y_i = []
+    
+    freq_range = range(Decimal('-0.2') * N_REF, (Decimal('0.8') * N_REF))
+    
+    for freq in freq_range:
+      y_i.append(sine(AMPLITUTDE, float(FREQ_STEP * freq), x_axis))
+    
+    z_i = []
+    for freq in freq_range:
+      z_i.append(cosine(AMPLITUTDE, float(FREQ_STEP * freq), x_axis))
+    
+    refs = y_i + z_i
+    
+    return x_axis, y, refs 
+  
+  def test_pseudo_fourier_decomposition(self):
+    x_axis, y, refs = self.prepare_fourier(0.0)
+    # computes decomposition
+    decomposition = pod.decompose(y, refs, epsilon=1E-6, max_iter=20)
+    approx = decomposition.get_decomposition()
+    comp_0 = decomposition.get_principal_component(0)
+    comp_1 = decomposition.get_principal_component(1)
+    comp_2 = decomposition.get_principal_component(2)
+    
+    self.assertEqual(decomposition.get_principal_component_index(0), 4)
+    self.assertEqual(decomposition.get_principal_component_index(1), 10)
+    self.assertEqual(decomposition.get_principal_component_index(2), 15)
+
+  def test_pseudo_fourier_decomposition_shift(self):
+    x_axis, y, refs = self.prepare_fourier(-0.5 * math.pi)
+    # computes decomposition
+    decomposition = pod.decompose(y, refs, epsilon=1E-6, max_iter=20)
+    approx = decomposition.get_decomposition()
+    comp_0 = decomposition.get_principal_component(0)
+    comp_1 = decomposition.get_principal_component(1)
+    comp_2 = decomposition.get_principal_component(2)
+            
+    self.assertEqual(decomposition.get_principal_component_index(0), 8)
+    self.assertEqual(decomposition.get_principal_component_index(1), 0)
+    self.assertEqual(decomposition.get_principal_component_index(2), 50)
+
 if __name__ == '__main__':
     unittest.main()
