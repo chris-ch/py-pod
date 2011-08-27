7ea9e9608ebb70614746a99990a56418bd330ce2
chris.perso@gmail.com <chris.perso@gmail.com@986c3d7b-1ff4-b555-b2c5-4a075b755c62>
2011-08-27 20:42:37 +0000

    dev

--- pod/mathtools.py
+++ pod/mathtools.py
@@ -3,35 +3,37 @@ Various functions operating on vectors.
 """
 
 import math
+from itertools import izip
 
 def scale(alpha, point):
-  return [(alpha * value) for value in point]
+  return ((alpha * value) for value in point)
 
 def sum_product(point1, point2):
   result = 0.0
-  for index, value1 in enumerate(point1):
-    result += value1 * point2[index]
+  for value1, value2 in izip(point1, point2):
+    result += value1 * value2
+    
   return result
   
 def vadd(point1, point2):
-  return [(v1 + v2) for v1, v2 in zip(point1, point2)]
+  return [(v1 + v2) for v1, v2 in izip(point1, point2)]
 
 def vsub(point1, point2):
-  return [(v1 - v2) for v1, v2 in zip(point1, point2)]
+  return [(v1 - v2) for v1, v2 in izip(point1, point2)]
   
 def norm(point):
   return math.sqrt(sum_product(point, point))
 
 def units(point, unit_vector):
-    """
-    How many times a point fits in the specified units (in norm).
+  """
+  How many times a point fits in the specified units (in norm).
+  
+  The sign has a meaning only if both vectors are colinear.
+  """
+  ratio = norm(point) / norm(unit_vector)
+  if norm(vsub(point, unit_vector)) > norm(unit_vector):
+    # vectors are in opposite directions
+    ratio = -ratio
     
-    The sign has a meaning only if both vectors are colinear.
-    """
-    ratio = norm(point) / norm(unit_vector)
-    if norm(vsub(point, unit_vector)) > norm(unit_vector):
-      # vectors are in opposite directions
-      ratio = -ratio
-      
-    return ratio
+  return ratio
   
